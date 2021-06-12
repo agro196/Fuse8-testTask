ab8a225028b3c640086b4fa02ccac7089e4edfbd
Agro <agro_06@mail.ru>
2021-06-12 16:50:53 +0300

    Build was created

--- src/components/Button/Button.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import cx from "clsx";
-import "./Button.scss";
-
-export function Button({ className, ...props }) {
-  return <button className={cx(className, "btn")} type="button" {...props} />;
-}
